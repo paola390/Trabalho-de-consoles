0c24fa36b6b01d46e2b6827594ecbf602be4b0e8
paola390 <paolettinha29@gmail.com>
2023-09-04 20:12:12 -0300

    merge com conflito

--- namespace HelloWorld.cs
+++ namespace HelloWorld.cs
@@ -3,12 +3,8 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
-            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!!!!");
             System.Console.WriteLine("Hello World!2");
-=======
-            System.Console.WriteLine("Hello World!MUNDOOOO");
->>>>>>> Stashed changes
         }
     }
 }
